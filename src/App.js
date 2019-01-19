47fa0bd5fe40de9fd21a37de48697736c13ad4e5
Robbie Gifford <robbie.gifford@gmail.com>
2019-01-19 14:41:30 -0800

    Render Google Map to fill view port

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
+import _ from "lodash";
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
-}
+const googleMapAPIRoot = 'https://maps.googleapis.com/maps/api/js'
 
-export default App;
+// Google Maps API url w/ querystrings
+const googleMapURL = googleMapAPIRoot.concat('?', 
+    // Google Maps API key
+    'key=', googleMapAPIKey, '&',
+    // Google Maps API libraries
+    'libraries=places')
+
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props => {
+    return (
+  <GoogleMap defaultZoom={12} defaultCenter={{ lat: 34.0522, lng: -118.2437 }}>
+    <Marker position={{ lat: -34.397, lng: 150.644 }} />
+  </GoogleMap>)
+});
+
+const enhance = _.identity;
+
+const ReactGoogleMaps = () => [
+  <MyMapComponent key="map" />
+];
+
+export default enhance(ReactGoogleMaps);
